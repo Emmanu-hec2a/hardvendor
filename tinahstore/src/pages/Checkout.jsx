407bca6c56e2d61876e178b9b608623732bd4ce3
Emmanuel Odongo <odiwourmanu123@gmail.com>
2026-09-14 15:13:52 +0300

    Updates: Login 400 error

--- tinahstore/src/pages/Checkout.jsx
+++ tinahstore/src/pages/Checkout.jsx
@@ -150,8 +150,7 @@ export default function Checkout() {
                     <p><b>Instructions:</b></p>
                     <p style={{ marginTop: 6 }}>1. Send the 60% deposit (<b>{formatKes(cart.total * 0.6)}</b>) to the number below:</p>
                     <div style={{ marginTop: 10, padding: '10px', background: 'var(--surface)', borderRadius: 6, border: '1px solid var(--hairline)' }}>
-                      <p><b>M-PESA:</b> 0717272726 (CHRISTINE)</p>
-                      <p><b>Airtel Money:</b> 0750243752 (CHRISTINE)</p>
+                      <p><b>M-PESA:</b> 0717272726 (NIGEL)</p>
                     </div>
                     <p style={{ marginTop: 12 }}>2. Enter your <b>Transaction Code</b> below (e.g., RJL1234567):</p>
                     <input
